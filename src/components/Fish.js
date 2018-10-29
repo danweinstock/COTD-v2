7680dec2e2c670e3dd2f4831624d9101f9eb191e
Dan Weinstock <dweinstock@kinjunxion.com>
2018-10-29 13:05:31 -0400

    Added Validation by Proptype

--- src/components/Fish.js
+++ src/components/Fish.js
@@ -1,24 +1,40 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { formatPrice } from "../helpers";
 
 class Fish extends React.Component {
-
+  static propTypes = {
+    // shape is function that accepts object
+    details: PropTypes.shape({
+      image: PropTypes.string,
+      name: PropTypes.string,
+      desc: PropTypes.string,
+      status: PropTypes.string,
+      price: PropTypes.number
+    }),
+    addToOrder: PropTypes.func
+  };
 
   render() {
-    const {image, name, desc, price, status} = this.props.details;
-    const isAvailable = status === 'available';
-  
+    const { image, name, desc, price, status } = this.props.details;
+    const isAvailable = status === "available";
+
     return (
       <li className="menu-fish">
-        <img src={image} alt={name}/>
-        <h3 className="fish-name">{name}
+        <img src={image} alt={name} />
+        <h3 className="fish-name">
+          {name}
           <span className="price">{formatPrice(price)}</span>
         </h3>
         <p>{desc}</p>
-        <button disabled={!isAvailable} onClick={() => this.props.addToOrder(this.props.index)}>
-          {isAvailable ? 'Add To Order': 'Sold Out!'}</button>
+        <button
+          disabled={!isAvailable}
+          onClick={() => this.props.addToOrder(this.props.index)}
+        >
+          {isAvailable ? "Add To Order" : "Sold Out!"}
+        </button>
       </li>
-    )
+    );
   }
 }
 
